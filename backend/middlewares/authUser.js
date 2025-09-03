f3250dc2bf4147e0e31a3b5bf5e811f453878723
Dhiraj-8969 <8969dhiraj@gmail.com>
2025-09-03 11:31:21 +0530

    Stop tracking admin/.env

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -1,20 +1,20 @@
 import jwt from "jsonwebtoken";
 
-const authUser= async(req,res,next)=>{
-  try{
+const authUser = async (req, res, next) => {
+  try {
 
     const { token } = req.headers
     if (!token) {
-      return res.json({success: false, message:"Not Authrized Login Again"})
+      return res.json({ success: false, message: "Not Authrized Login Again" })
     }
-    const token_decode=jwt.verify(token,process.env.JWT_SECRET)
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET)
 
     req.body.userId = token_decode.id
     next()
 
-  }catch(error){
-  console.log(error)
-  res.json({success: false, message:error.message})
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
   }
 }
 
